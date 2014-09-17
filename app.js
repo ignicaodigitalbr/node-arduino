ac17a5f09a7fca76c6e3c36271375ee0d88adf73
Vitor Nogueira <vnweb.contato@gmail.com>
2014-09-17 00:36:01 -0300

    Adicionando mais leds

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var board = new five.Board();
 
 var boardIsReady = false;
 var leds = {};
+var currentStates = [];
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -24,17 +25,50 @@ board.on('ready', function () {
 
   io.emit('statusBoard', boardIsReady);
 
-  leds.led9 = new five.Led(9);
-  leds.led11 = new five.Led(11);
-  leds.led13 = new five.Led(13);
+  leds.led7 = new five.Pin(7);
+  leds.led9 = new five.Pin(9);
+  leds.led11 = new five.Pin(11);
+  leds.led13 = new five.Pin(13);
 });
 
 io.on('connection', function (socket) {
   io.emit('statusBoard', boardIsReady);
 
-  socket.on('led', function (led) {
+  if (boardIsReady) {
+    leds.led7.high();
+
+    leds.led9.query(function (state) {
+      io.emit('changeLed', {
+        led: 'led9',
+        state: state.value
+      });
+    });
+
+    leds.led11.query(function (state) {
+      io.emit('changeLed', {
+        led: 'led11',
+        state: state.value
+      });
+    });
+
+    leds.led13.query(function (state) {
+      io.emit('changeLed', {
+        led: 'led13',
+        state: state.value
+      });
+    });
+  }
+
+  socket.on('led', function (data) {
     if (boardIsReady) {
-      leds[led].toggle();
+      if (data.state) {
+        leds[data.led].high();
+      }
+      else {
+        leds[data.led].low();
+      }
+
+      io.emit('changeLed', data);
     }
   });
 });
